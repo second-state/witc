e47002b9efc38d4377484e6958023ef2e2f2ab23
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2022-11-29 11:01:13 +0800

    `check` command
    Co-authored-by: dm4 <dm4@users.noreply.github.com>
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main (main) where
 
+import System.Directory
 import System.Environment
+import Text.Megaparsec
+import Wit.Check
 import Wit.Parser
 
 -- cli design
@@ -14,9 +17,26 @@ main = do
   handle args
 
 handle :: [String] -> IO ()
+handle ["check"] = do
+  dir <- getCurrentDirectory
+  fileList <- listDirectory dir
+  mapM_ checkFile fileList
+  return ()
 handle ["instance", mode, file] = do
   -- ast <- parse file
   -- output <- gen mode ast
   putStrLn "TODO"
 handle ["runtime", mode, file] = putStrLn "TODO"
 handle _ = putStrLn "bad usage"
+
+checkFile :: FilePath -> IO ()
+checkFile filepath = do
+  contents <- readFile filepath
+  case parse pWitFile filepath contents of
+    Left bundle -> putStr (errorBundlePretty bundle)
+    Right wit_file -> do
+      r <- check0 wit_file
+      case r of
+        -- TODO: hint source code via position
+        Left (msg, _pos) -> putStrLn msg
+        Right () -> return ()
