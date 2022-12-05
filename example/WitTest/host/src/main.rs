66ef6714e7573993724631ae894b1754728f4e46
dm4 <dm4@secondstate.io>
2022-12-05 10:41:35 +0800

    add example/WitTest
    Co-authored-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    Signed-off-by: dm4 <dm4@secondstate.io>

---
+++ example/WitTest/host/src/main.rs
@@ -0,0 +1,54 @@
+use anyhow::Error;
+use wasmedge_sdk::{
+    config::{CommonConfigOptions, ConfigBuilder, HostRegistrationConfigOptions},
+    error::HostFuncError,
+    host_function, Caller, ImportObjectBuilder, Vm, WasmValue,
+};
+
+fn load_string(caller: &Caller, addr: u32, size: u32) -> String {
+    let mem = caller.memory(0).unwrap();
+    let data = mem.read(addr, size).expect("fail to get string");
+    String::from_utf8_lossy(&data).to_string()
+}
+
+#[host_function]
+fn exchange(caller: Caller, input: Vec<WasmValue>) -> Result<Vec<WasmValue>, HostFuncError> {
+    let addr = input[0].to_i32() as u32;
+    let cap = input[1].to_i32() as u32;
+    let size = input[2].to_i32() as u32;
+    let s = load_string(&caller, addr, size);
+    println!("Rust: Get: {}", s);
+
+    let mut mem = caller.memory(0).unwrap();
+    // take last address+1
+    let final_addr = mem.size() + 1;
+    // grow a page size
+    mem.grow(1).expect("fail to grow memory");
+    // put the returned string into new address
+    mem.write(s.as_bytes(), final_addr)
+        .expect("fail to write returned string");
+
+    Ok(vec![
+        WasmValue::from_i32(final_addr as i32),
+        WasmValue::from_i32(s.capacity() as i32),
+        WasmValue::from_i32(s.len() as i32),
+    ])
+}
+
+fn main() -> Result<(), Error> {
+    let config = ConfigBuilder::new(CommonConfigOptions::default())
+        .with_host_registration_config(HostRegistrationConfigOptions::default().wasi(true))
+        .build()?;
+
+    let import = ImportObjectBuilder::new()
+        .with_func::<(i32, i32, i32), (i32, i32, i32)>("exchange", exchange)?
+        .build("host")?;
+    let vm = Vm::new(Some(config))?
+        .register_import_module(import)?
+        .register_module_from_file("lib", "target/wasm32-wasi/release/lib.wasm")?;
+
+    let result = vm.run_func(Some("lib"), "start", None)?;
+    println!("result: {}", result[0].to_i32());
+
+    Ok(())
+}
