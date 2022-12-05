7727ebe90f2f5f2589801010cb023eeeb6320552
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2022-12-06 01:46:38 +0800

    `expected` type init
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- example/WitTest/lib/src/lib.rs
+++ example/WitTest/lib/src/lib.rs
@@ -4,13 +4,23 @@ pmacro::wit_instance_import!("../test.wit");
 
 #[no_mangle]
 pub unsafe extern "wasm" fn start() -> u32 {
+    // string & struct (wit record)
     let mut s = String::with_capacity(10);
     s.push('a');
-    let p = person {
-        name: "Carlo".to_string(),
-        age: 30,
-    };
-    let _s = exchange(s, p);
+    let _s = exchange(
+        s,
+        person {
+            name: "Carlo".into(),
+            age: 30,
+        },
+    );
+    // enum (wit enum)
     let _i = exchange_enum(color::green);
+    // result (wit expected)
+    let r = Ok(321);
+    let _r = handle_result(r);
+    let r = Err("test".into());
+    let _r = handle_result(r);
+
     return 0;
 }
