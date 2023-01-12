9dd3118d05efb3dff8f8a50f8b06a8604dcead64
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-01-12 11:30:43 +0800

    introduce quote string & reduce the gen code
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- src/Wit/Gen.hs
+++ src/Wit/Gen.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Wit.Gen
   ( renderInstanceImport,
     renderRuntimeExport,
@@ -7,6 +9,7 @@ where
 import Data.List (partition)
 import Prettyprinter
 import Prettyprinter.Render.Text
+import QStr
 import Wit.Ast
 import Wit.Gen.Export
 import Wit.Gen.Import
@@ -35,39 +38,68 @@ data Config = Config
     side :: Side
   }
 
+rustAsRemoteString :: String
+rustAsRemoteString =
+  [str|
+fn as_remote_string<A>(a: A) -> (usize, usize)
+where A: Serialize,
+{
+	let s = serde_json::to_string(&a).unwrap();
+	let remote_addr = unsafe { allocate(s.len() as usize) };
+	unsafe {
+		for (i, c) in s.bytes().enumerate() {
+			write(remote_addr, i, c);
+		}
+	}
+	(remote_addr, s.len())
+}
+|]
+
+rustFromRemoteString :: String
+rustFromRemoteString =
+  [str|
+fn from_remote_string(pair: (usize, usize)) -> String {
+	let (remote_addr, len) = pair;
+	let mut s = String::with_capacity(len);
+	unsafe {
+		for i in 0..len {
+			s.push(read(remote_addr, i) as char);
+		}
+	}
+	s
+}
+|]
+
 prettyFile :: Config -> WitFile -> Doc a
 prettyFile config WitFile {definition_list = def_list} =
   let (ty_defs, defs) = partition isTypeDef def_list
    in case (config.side, config.direction) of
         (Instance, Import) ->
-          vsep (map prettyTypeDef ty_defs)
-            <+> line
-            <+> pretty "fn as_remote_string<A>(a: A) -> (usize, usize) where A: Serialize, { let s = serde_json::to_string(&a).unwrap(); let remote_addr = unsafe { allocate(s.len() as usize) }; unsafe { for (i, c) in s.bytes().enumerate() { write(remote_addr, i, c); } } (remote_addr, s.len()) } fn from_remote_string(pair: (usize, usize)) -> String { let (remote_addr, len) = pair; let mut s = String::with_capacity(len); unsafe { for i in 0..len { s.push(read(remote_addr, i) as char); } } s }"
-            <+> line
-            <+> pretty "#[link(wasm_import_module = \"wasmedge\")]"
-            <+> line
-            <+> pretty "extern \"wasm\""
-            <+> braces
-              ( line
-                  <+> indent
-                    4
-                    ( vsep $
-                        map
-                          pretty
-                          [ "fn allocate(size: usize) -> usize;",
-                            "fn write(addr: usize, offset: usize, byte: u8);",
-                            "fn read(addr: usize, offset: usize) -> u8;"
-                          ]
-                          ++ map prettyDefExtern defs
-                    )
-                  <+> line
-              )
-            <+> line
-            <+> vsep (map prettyDefWrap defs)
+          vsep $
+            map prettyTypeDef ty_defs
+              ++ [ pretty rustAsRemoteString,
+                   pretty rustFromRemoteString,
+                   pretty "#[link(wasm_import_module = \"wasmedge\")]",
+                   pretty "extern \"wasm\"",
+                   braces
+                     ( line
+                         <+> indent
+                           4
+                           ( vsep $
+                               map
+                                 pretty
+                                 [ "fn allocate(size: usize) -> usize;",
+                                   "fn write(addr: usize, offset: usize, byte: u8);",
+                                   "fn read(addr: usize, offset: usize) -> u8;"
+                                 ]
+                                 ++ map prettyDefExtern defs
+                           )
+                         <+> line
+                     )
+                 ]
+              ++ map prettyDefWrap defs
         (Runtime, Export) ->
-          vsep (map prettyTypeDef ty_defs)
-            <+> line
-            <+> vsep (map toHostFunction defs)
+          vsep (map prettyTypeDef ty_defs ++ map toHostFunction defs)
             <+> witObject defs
         (_, _) -> error "unsupported side, direction combination"
 
