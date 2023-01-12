92c989235ee9507b68de4528f82971f755e73730
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-01-12 13:39:16 +0800

    we always can wrap functions into proper abstraction in language
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- example/keyvalue-demo/instance-client/src/lib.rs
+++ example/keyvalue-demo/instance-client/src/lib.rs
@@ -2,11 +2,31 @@
 use serde::{Deserialize, Serialize};
 invoke_witc::wit_instance_import!("./keyvalue.wit");
 
+struct Store {
+    handle: keyvalue,
+}
+
+impl Store {
+    fn open(name: String) -> Self {
+        Self {
+            handle: open_store(name).unwrap(),
+        }
+    }
+
+    fn set(&self, key: String, value: Vec<u8>) {
+        store_set(self.handle, key, value).unwrap();
+    }
+
+    fn get(&self, key: String) -> Vec<u8> {
+        store_get(self.handle, key).unwrap()
+    }
+}
+
 #[no_mangle]
 pub unsafe extern "wasm" fn start() -> u32 {
-    let store = open_store("store A".to_string()).unwrap();
-    store_set(store, "key1".to_string(), vec![1, 2, 3]).unwrap();
-    let value = store_get(store, "key1".to_string()).unwrap();
+    let store = Store::open("store A".to_string());
+    store.set("key1".to_string(), vec![1, 2, 3]);
+    let value = store.get("key1".to_string());
     assert_eq!(value, vec![1, 2, 3]);
     return 0;
 }
