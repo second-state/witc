68edb1503594bb7fb9fd436d4f4039f693ea1a97
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-02-02 16:02:58 +0800

    test fib function & introduce blackbox
    blackbox can avoid optmization makes incorrect benchmarking
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- bench/instance/src/lib.rs
+++ bench/instance/src/lib.rs
@@ -3,11 +3,16 @@ use serde::{Deserialize, Serialize};
 invoke_witc::wit_instance!(import("base.wit"));
 
 #[no_mangle]
-pub unsafe extern "wasm" fn start() -> u32 {
+pub unsafe extern "wasm" fn call_base() -> u32 {
     let arg = c {
         name: "test".to_string(),
         age: 1,
     };
     let r = base(arg);
     return r.age;
+}
+
+#[no_mangle]
+pub unsafe extern "wasm" fn call_fib() -> u64 {
+    return fib(10);
 }
--- bench/runtime/src/lib.rs
+++ bench/runtime/src/lib.rs
@@ -12,6 +12,13 @@ fn base(c1: c) -> c {
     }
 }
 
+fn fib(n: u64) -> u64 {
+    if n <= 1 {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,18 +28,7 @@ mod tests {
         Vm,
     };
 
-    #[bench]
-    fn base_native(b: &mut Bencher) {
-        b.iter(|| {
-            base(c {
-                name: "test".to_string(),
-                age: 1,
-            });
-        });
-    }
-
-    #[bench]
-    fn base_instance_invokes_runtime(b: &mut Bencher) {
+    fn test_vm() -> Vm {
         let config = ConfigBuilder::new(CommonConfigOptions::default())
             .with_host_registration_config(HostRegistrationConfigOptions::default().wasi(true))
             .build()
@@ -40,15 +36,50 @@ mod tests {
 
         let import_object = wit_import_object().unwrap();
 
-        let vm = Vm::new(Some(config))
+        Vm::new(Some(config))
             .unwrap()
             .register_import_module(import_object)
             .unwrap()
             .register_module_from_file("instance", "../target/wasm32-wasi/release/instance.wasm")
-            .unwrap();
+            .unwrap()
+    }
+
+    #[bench]
+    fn base_native(b: &mut Bencher) {
+        b.iter(|| {
+            let n = test::black_box(1000);
+
+            base(c {
+                name: "test".to_string(),
+                age: n,
+            });
+        });
+    }
+
+    #[bench]
+    fn base_instance_invokes_runtime(b: &mut Bencher) {
+        let vm = test_vm();
 
         b.iter(|| {
-            vm.run_func(Some("instance"), "start", None).unwrap();
+            vm.run_func(Some("instance"), "call_base", None).unwrap();
+        });
+    }
+
+    #[bench]
+    fn fib_native(b: &mut Bencher) {
+        b.iter(|| {
+            let n = test::black_box(10);
+
+            fib(n);
+        });
+    }
+
+    #[bench]
+    fn fib_instance_invokes_runtime(b: &mut Bencher) {
+        let vm = test_vm();
+
+        b.iter(|| {
+            vm.run_func(Some("instance"), "call_fib", None).unwrap();
         });
     }
 }
