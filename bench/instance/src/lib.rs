3a14c8d8d85f94e590da5ecc4ca7ac66b4864b46
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-02-02 16:22:54 +0800

    adding host_function benchmarking
    This shows our wasm invocation is expensive
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- bench/instance/src/lib.rs
+++ bench/instance/src/lib.rs
@@ -2,6 +2,11 @@
 use serde::{Deserialize, Serialize};
 invoke_witc::wit_instance!(import("base.wit"));
 
+#[link(wasm_import_module = "host")]
+extern "wasm" {
+    fn host_fib(n: u64) -> u64;
+}
+
 #[no_mangle]
 pub unsafe extern "wasm" fn call_base() -> u32 {
     let arg = c {
@@ -15,4 +20,9 @@ pub unsafe extern "wasm" fn call_base() -> u32 {
 #[no_mangle]
 pub unsafe extern "wasm" fn call_fib() -> u64 {
     return fib(10);
+}
+
+#[no_mangle]
+pub unsafe extern "wasm" fn call_host_fib() -> u64 {
+    return host_fib(10);
 }
--- bench/runtime/src/lib.rs
+++ bench/runtime/src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(test)]
 extern crate test;
 use serde::{Deserialize, Serialize};
-use wasmedge_sdk::{host_function, Caller};
+use wasmedge_sdk::{error::HostFuncError, host_function, Caller, WasmValue};
 use witc_abi::*;
 invoke_witc::wit_runtime!(export("base.wit"));
 
@@ -19,13 +19,20 @@ fn fib(n: u64) -> u64 {
     return fib(n - 1) + fib(n - 2);
 }
 
+#[host_function]
+fn host_fib(_: Caller, values: Vec<WasmValue>) -> Result<Vec<WasmValue>, HostFuncError> {
+    let v = values[0].to_i64();
+    let r = fib(v as u64);
+    Ok(vec![WasmValue::from_i64(r as i64)])
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use test::Bencher;
     use wasmedge_sdk::{
         config::{CommonConfigOptions, ConfigBuilder, HostRegistrationConfigOptions},
-        Vm,
+        ImportObjectBuilder, Vm,
     };
 
     fn test_vm() -> Vm {
@@ -36,9 +43,17 @@ mod tests {
 
         let import_object = wit_import_object().unwrap();
 
+        let i2 = ImportObjectBuilder::new()
+            .with_func::<i64, i64>("host_fib", host_fib)
+            .unwrap()
+            .build("host")
+            .unwrap();
+
         Vm::new(Some(config))
             .unwrap()
             .register_import_module(import_object)
+            .unwrap()
+            .register_import_module(i2)
             .unwrap()
             .register_module_from_file("instance", "../target/wasm32-wasi/release/instance.wasm")
             .unwrap()
@@ -80,6 +95,16 @@ mod tests {
 
         b.iter(|| {
             vm.run_func(Some("instance"), "call_fib", None).unwrap();
+        });
+    }
+
+    #[bench]
+    fn fib_instance_invokes_host_function(b: &mut Bencher) {
+        let vm = test_vm();
+
+        b.iter(|| {
+            vm.run_func(Some("instance"), "call_host_fib", None)
+                .unwrap();
         });
     }
 }
