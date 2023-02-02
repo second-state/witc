30e869024ed621989948d664d86b18ae55d23b31
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-02-02 15:29:58 +0800

    cleanup, no need to have an executable here
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- bench/runtime/src/lib.rs
+++ bench/runtime/src/lib.rs
@@ -1,12 +1,7 @@
 #![feature(test)]
 extern crate test;
-
-use anyhow::Error;
 use serde::{Deserialize, Serialize};
-use wasmedge_sdk::{
-    config::{CommonConfigOptions, ConfigBuilder, HostRegistrationConfigOptions},
-    host_function, Caller, Vm,
-};
+use wasmedge_sdk::{host_function, Caller};
 use witc_abi::*;
 invoke_witc::wit_runtime!(export("base.wit"));
 
@@ -17,25 +12,14 @@ fn base(c1: c) -> c {
     }
 }
 
-fn main() -> Result<(), Error> {
-    let config = ConfigBuilder::new(CommonConfigOptions::default())
-        .with_host_registration_config(HostRegistrationConfigOptions::default().wasi(true))
-        .build()?;
-
-    let vm = Vm::new(Some(config))?
-        .register_import_module(wit_import_object()?)?
-        .register_module_from_file("instance", "target/wasm32-wasi/release/instance.wasm")?;
-
-    let result = vm.run_func(Some("instance"), "start", None)?;
-    assert!(result[0].to_i32() == 2);
-
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use test::Bencher;
+    use wasmedge_sdk::{
+        config::{CommonConfigOptions, ConfigBuilder, HostRegistrationConfigOptions},
+        Vm,
+    };
     #[bench]
     fn b1(b: &mut Bencher) {
         let config = ConfigBuilder::new(CommonConfigOptions::default())
