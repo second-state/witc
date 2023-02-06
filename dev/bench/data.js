4375edb34b16195947effe5374143d12281b06c7
github-action-benchmark <github@users.noreply.github.com>
2023-02-06 10:58:45 +0000

    add Rust Benchmark (cargo) benchmark result for 808ed22bd25527f1bfe2ccc13ab60df7892edd73

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675680074059,
+  "lastUpdate": 1675681125951,
   "repoUrl": "https://github.com/second-state/witc",
   "entries": {
     "Rust Benchmark": [
@@ -43,6 +43,56 @@ window.BENCHMARK_DATA = {
             "name": "tests::fib_instance_invokes_runtime",
             "value": 14753,
             "range": "± 112",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_native",
+            "value": 172,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "second-state",
+            "username": "second-state"
+          },
+          "committer": {
+            "name": "second-state",
+            "username": "second-state"
+          },
+          "id": "808ed22bd25527f1bfe2ccc13ab60df7892edd73",
+          "message": "fix benchmark ci",
+          "timestamp": "2023-02-06T08:45:22Z",
+          "url": "https://github.com/second-state/witc/pull/58/commits/808ed22bd25527f1bfe2ccc13ab60df7892edd73"
+        },
+        "date": 1675681125485,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::base_instance_invokes_runtime",
+            "value": 90352,
+            "range": "± 767",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::base_native",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_instance_invokes_host_function",
+            "value": 2524,
+            "range": "± 58",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_instance_invokes_runtime",
+            "value": 14850,
+            "range": "± 89",
             "unit": "ns/iter"
           },
           {
