839597aa6f566e8c3afc9f921ee19d6a2c4064da
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-03-13 10:11:04 +0800

    print witc version out
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,7 @@ import System.Environment
 import Wit
 
 handle :: [String] -> IO ()
+handle ["version"] = putStrLn "0.1.0"
 handle ["check", file] = checkFileWithDoneHint file
 handle ["check"] = do
   dir <- getCurrentDirectory
