e1dc8dcc5bd4351a9401bd82e476c65dd634e1f1
github-action-benchmark <github@users.noreply.github.com>
2023-03-13 02:15:33 +0000

    add Rust Benchmark (cargo) benchmark result for 66e15a365c4c4047312185f075397f3eff8233eb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678275104997,
+  "lastUpdate": 1678673733301,
   "repoUrl": "https://github.com/second-state/witc",
   "entries": {
     "Rust Benchmark": [
@@ -865,6 +865,56 @@ window.BENCHMARK_DATA = {
             "name": "tests::fib_native",
             "value": 187,
             "range": "± 22",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "second-state",
+            "username": "second-state"
+          },
+          "committer": {
+            "name": "second-state",
+            "username": "second-state"
+          },
+          "id": "66e15a365c4c4047312185f075397f3eff8233eb",
+          "message": "changes for cargo publish ",
+          "timestamp": "2023-03-02T06:33:33Z",
+          "url": "https://github.com/second-state/witc/pull/67/commits/66e15a365c4c4047312185f075397f3eff8233eb"
+        },
+        "date": 1678673732720,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::base_instance_invokes_runtime",
+            "value": 92839,
+            "range": "± 1084",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::base_native",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_instance_invokes_host_function",
+            "value": 2556,
+            "range": "± 18",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_instance_invokes_runtime",
+            "value": 14291,
+            "range": "± 316",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_native",
+            "value": 198,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
