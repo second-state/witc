43d3dd7a2852136c43008cd80ab8020f3ec730f0
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-03-17 14:51:15 +0800

    extract `codegen`
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- app/Main.hs
+++ app/Main.hs
@@ -18,42 +18,32 @@ import Wit
 
 handle :: [String] -> IO ()
 handle ["version"] = putStrLn "0.2.0"
+-- validation
 handle ["check", file] = checkFileWithDoneHint file
 handle ["check"] = do
   dir <- getCurrentDirectory
   witFileList <- filter (".wit" `isSuffixOf`) <$> listDirectory dir
   mapM_ checkFileWithDoneHint witFileList
-handle ["instance", "import", file, importName] =
-  parseFile file
-    >>= eitherIO check0
-    >>= eitherIO (putDoc . prettyFile Config {language = Rust, direction = Import, side = Instance} importName)
-handle ["runtime", "import", file, importName] =
-  parseFile file
-    >>= eitherIO check0
-    >>= eitherIO (putDoc . prettyFile Config {language = Rust, direction = Import, side = Runtime} importName)
+-- codegen
+handle ["instance", "import", file, importName] = codegen file Import Instance importName
+handle ["runtime", "import", file, importName] = codegen file Import Runtime importName
 handle ["instance", mode, file] = do
   case mode of
-    "import" ->
-      parseFile file
-        >>= eitherIO check0
-        >>= eitherIO (putDoc . prettyFile Config {language = Rust, direction = Import, side = Instance} "wasmedge")
-    "export" ->
-      parseFile file
-        >>= eitherIO check0
-        >>= eitherIO (putDoc . prettyFile Config {language = Rust, direction = Export, side = Instance} "wasmedge")
+    "import" -> codegen file Import Instance "wasmedge"
+    "export" -> codegen file Export Instance "wasmedge"
     bad -> putStrLn $ "unknown option: " ++ bad
 handle ["runtime", mode, file] =
   case mode of
-    "import" ->
-      parseFile file
-        >>= eitherIO check0
-        >>= eitherIO (putDoc . prettyFile Config {language = Rust, direction = Import, side = Runtime} "wasmedge")
-    "export" ->
-      parseFile file
-        >>= eitherIO check0
-        >>= eitherIO (putDoc . prettyFile Config {language = Rust, direction = Export, side = Runtime} "wasmedge")
+    "import" -> codegen file Import Runtime "wasmedge"
+    "export" -> codegen file Export Runtime "wasmedge"
     bad -> putStrLn $ "unknown option: " ++ bad
 handle _ = putStrLn "bad usage"
+
+codegen :: FilePath -> Direction -> Side -> String -> IO ()
+codegen file direction side importName =
+  parseFile file
+    >>= eitherIO check0
+    >>= eitherIO (putDoc . prettyFile Config {language = Rust, direction = direction, side = side} importName)
 
 checkFileWithDoneHint :: FilePath -> IO ()
 checkFileWithDoneHint file = do
