e69f4243c58368a2e983533b67ed4898ec79eaeb
github-action-benchmark <github@users.noreply.github.com>
2023-06-05 10:08:41 +0000

    add Rust Benchmark (cargo) benchmark result for 2c138cb6f382b01523d4cb80d30ce5fdff23c4b8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685959265979,
+  "lastUpdate": 1685959721255,
   "repoUrl": "https://github.com/second-state/witc",
   "entries": {
     "Rust Benchmark": [
@@ -5485,6 +5485,68 @@ window.BENCHMARK_DATA = {
             "name": "tests::fib_native",
             "value": 177,
             "range": "± 10",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "second-state",
+            "username": "second-state"
+          },
+          "committer": {
+            "name": "second-state",
+            "username": "second-state"
+          },
+          "id": "2c138cb6f382b01523d4cb80d30ce5fdff23c4b8",
+          "message": "New codegen",
+          "timestamp": "2023-05-23T15:01:43Z",
+          "url": "https://github.com/second-state/witc/pull/104/commits/2c138cb6f382b01523d4cb80d30ce5fdff23c4b8"
+        },
+        "date": 1685959720057,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::base_instance_invokes_instance",
+            "value": 76438,
+            "range": "± 11333",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::base_instance_invokes_runtime",
+            "value": 40386,
+            "range": "± 4931",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::base_native",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::base_runtime_invokes_instance",
+            "value": 36012,
+            "range": "± 6540",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_instance_invokes_host_function",
+            "value": 1520,
+            "range": "± 94",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_instance_invokes_runtime",
+            "value": 10698,
+            "range": "± 436",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tests::fib_native",
+            "value": 212,
+            "range": "± 19",
             "unit": "ns/iter"
           }
         ]
